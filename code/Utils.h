080f0abeb6e161a4860354b48043869ad5dab2fa
Billy1900 <32431813+Billy1900@users.noreply.github.com>
2019-10-07 15:43:20 +0800

    Update Utils.h

--- code/Utils.h
+++ code/Utils.h
@@ -1,3 +1,5 @@
+
+
 #ifndef TEST_UTILS_H
 #define TEST_UTILS_H
 
@@ -5,10 +7,13 @@
 #include <ctime>
 using namespace std;
 
+
 void SPN_test();
 void SPN(unsigned short hex_x, unsigned short &res, unsigned int Key);
 void Key_arrange(int i,unsigned short &Kr,unsigned int Key);
 
 void LinearAttack_test();
+void Crack(unsigned short key_r16);
+void Dif_Attack_test();
 
 #endif //TEST_UTILS_H
