4598523fa72ef4efc33fdf60925fd9ef495de6bb
Billy1900 <32431813+Billy1900@users.noreply.github.com>
2019-10-08 22:07:16 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -6,7 +6,9 @@ int main(){
     start = clock();
     //LinearAttack_test();
     //Dif_Attack_test();
-    SPN_Plus_test();
+    //SPN_Plus_test();
+    //RandTest();
     end = clock();
     cout<<(double)(end-start)<<"ms";
+
 }
